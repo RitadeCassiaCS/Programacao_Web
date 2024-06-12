1ce8b6e68f0c5411eeee9c3b1f75fca5178a8305
RitadeCassiaCS <ritadecassia_uni@hotmail.com>
2024-06-12 19:28:43 -0300

    Implementação da Atividade SOLID

---
+++ CICLO_03/SOLID/Original.php
@@ -0,0 +1,34 @@
+/*
+* Curso de Engenharia de Software - UniEVANGÉLICA 
+* Disciplina de Programação Web 
+* Dev: Rita de Cássia da Costa Silva
+* DATA: 12/06/2024
+*/ 
+
+class UserAccount
+{
+    public function createAccount($username, $password)
+    {
+        // lógica para criar conta
+    }
+
+    public function updateAccount($username, $password)
+    {
+        // lógica para atualizar conta
+    }
+
+    public function deleteAccount($username)
+    {
+        // lógica para deletar conta
+    }
+
+    public function sendWelcomeEmail($email)
+    {
+        // lógica para enviar e-mail de boas-vindas
+    }
+
+    public function logActivity($activity)
+    {
+        // lógica para registrar atividade
+    }
+}
